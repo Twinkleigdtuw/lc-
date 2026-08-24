fca051b7e4f5a09fe4833751ed779374bd61868e
Twinkle <twinkle141106@gmail.com>
2026-08-24 22:21:41 +0530

    08-24-2026

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            // odd length palindrome
+            int l = i, r = i;
+
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                ans++;
+                l--;
+                r++;
+            }
+
+            // even length palindrome
+            l = i;
+            r = i + 1;
+
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                ans++;
+                l--;
+                r++;
+            }
+        }
+
+        return ans;
+    }
+};
